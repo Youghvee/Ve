355229cd57c9ad1ae1360fd1f5377c4d2097b1db
Ryn Lulzsec <152095647+Youghvee@users.noreply.github.com>
2024-02-03 03:19:02 +0700

    Update play.py

--- AlexaMusic/utils/inline/play.py
+++ AlexaMusic/utils/inline/play.py
@@ -52,7 +52,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 callback_data=f"add_playlist {videoid}",
             ),
             InlineKeyboardButton(
-                text="Owners", url=f"https://t.me/Usern4meDoesNotExist404"
+                text="Owners", url=f"https://t.me/thvrxv"
             ),
         ],
         [
@@ -86,7 +86,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="Owners", url=f"https://t.me/Usern4meDoesNotExist404"),
+            InlineKeyboardButton(text="Owners", url=f"https://t.me/Thvrxv"),
         ],
         [
             InlineKeyboardButton(
@@ -116,7 +116,7 @@ def stream_markup(_, videoid, chat_id):
                 text=_["PL_B_3"],
                 callback_data=f"PanelMarkup None|{chat_id}",
             ),
-            InlineKeyboardButton(text="Owners", url=f"https://t.me/Usern4meDoesNotExist404"),
+            InlineKeyboardButton(text="Owners", url=f"https://t.me/Thvrxv"),
         ],
     ]
     return buttons
